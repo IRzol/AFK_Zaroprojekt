8ae8ffe053936cf85fe9fb4217026e4c6c57877a
KRoli <kocsisr1@kkszki.hu>
2026-05-21 12:20:53 +0200

    Ugrás kész

--- AFK_Zaroprojekt/Assets/Scripts/Player/PlayerMovement.cs
+++ AFK_Zaroprojekt/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
     private Rigidbody2D rb;
+    public bool isGrounded;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -13,5 +20,15 @@ public class PlayerMovement : MonoBehaviour
     {
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 }
