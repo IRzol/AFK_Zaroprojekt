969d2a703247c1757de31973f385f35e9fcc5e7c
AdamNandor <adamn@kkszki.hu>
2026-05-25 17:53:39 +0200

    rocket script

---
+++ AFK_Zaroprojekt/Assets/Scripts/Gun/Rocket.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+
+        if (!other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
