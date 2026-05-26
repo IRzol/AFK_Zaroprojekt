e8c8e2d470e65f4a873b842c9b46e19027e3a353
KRoli <kocsisr1@kkszki.hu>
2026-05-26 23:00:32 +0200

    ajto animacio kesz

--- AFK_Zaroprojekt/Assets/Scripts/Player/OpenDoor.cs
+++ AFK_Zaroprojekt/Assets/Scripts/Player/OpenDoor.cs
@@ -4,16 +4,21 @@ using UnityEngine.Tilemaps;
 public class Door : MonoBehaviour
 {
     public GameObject doorHitb;
+    public Animator animator;
     public float interactRange = 2f;
     public Transform player;
+    private bool opened = false;
 
     void Update()
     {
-        float distance = Vector2.Distance(transform.position, player.position);
+        if (opened) return;
 
+        float distance = Vector2.Distance(transform.position, player.position);
         if (distance <= interactRange && Input.GetKeyDown(KeyCode.E))
         {
             doorHitb.GetComponent<TilemapCollider2D>().enabled = false;
+            animator.SetBool("IsOpen", true); //
+            opened = true;
         }
     }
 }
