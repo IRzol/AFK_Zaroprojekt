7d96df3186537416c9dcf989b3b598eeeb565a92
AdamNandor <adamn@kkszki.hu>
2026-05-30 15:15:51 +0200

    arra nez a lovedek amerre kilovi a fegyver

--- AFK_Zaroprojekt/Assets/Scripts/Gun/Shooting.cs
+++ AFK_Zaroprojekt/Assets/Scripts/Gun/Shooting.cs
@@ -17,12 +17,12 @@ public class Shooting : MonoBehaviour
 
     void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = firePoint.position.z;
         Vector2 direction = ((Vector2)mousePos - (Vector2)firePoint.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.Euler(0, 0, angle));
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.linearVelocity = direction * bulletSpeed;
 
         Destroy(bullet, 3f);
