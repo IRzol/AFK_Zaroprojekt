208a0f7d63fe6ad75d7163e9f6e9328ce59fcc3b
FejesM1 <fejesm@kkszki.hu>
2026-06-04 08:41:36 +0200

    megall

--- AFK_Zaroprojekt/Assets/Scripts/uimanager.cs
+++ AFK_Zaroprojekt/Assets/Scripts/uimanager.cs
@@ -8,15 +8,21 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
-
         if (gameManager == null || settingsPanel == null)
+        {
+            Debug.Log("UIManager: gameManager vagy settingsPanel null!");
             return;
+        }
 
         if (!gameManager.gameStarted)
+        {
+            Debug.Log("UIManager: gameStarted = false, nem fut tovabb");
             return;
+        }
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            Debug.Log("UIManager: Escape lenyomva!");
             bool isOpen = !settingsPanel.activeSelf;
             settingsPanel.SetActive(isOpen);
             Time.timeScale = isOpen ? 0f : 1f;
